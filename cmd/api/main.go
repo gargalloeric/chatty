f21f5f514d73d86a62983841f596677b6480fbe7
eric <erickmontes555@gmail.com>
2025-07-20 13:14:41 +0200

    feat(api): add intial main function implementation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"time"
+)
+
+type config struct {
+	host string
+	port string
+	env  string
+}
+
+type application struct {
+	logger *slog.Logger
+	config config
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var conf config
+
+	flag.StringVar(&conf.host, "host", "", "Server host address")
+	flag.StringVar(&conf.port, "port", "3000", "Server port address")
+	flag.StringVar(&conf.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.Parse()
+
+	logger := slog.Default()
+
+	app := &application{
+		logger: logger,
+		config: conf,
+	}
+
+	server := &http.Server{
+		Addr:         net.JoinHostPort(conf.host, conf.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("server listening", "addr", net.JoinHostPort(conf.host, conf.port))
+
+	if err := server.ListenAndServe(); err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 }
