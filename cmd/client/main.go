4e33068e96af38aab4068a864b8e0507cef10d98
gargalloeric <erickmontes555@gmail.com>
2025-08-03 12:22:08 +0200

    feat(client): initial client implementation without server connection

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,112 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+const gap = "\n\n"
+
+type errorMsg error
+
+type model struct {
+	viewport    viewport.Model
+	messages    []string
+	textarea    textarea.Model
+	senderStyle lipgloss.Style
+	err         error
+}
+
+func initialModel() model {
+	ta := textarea.New()
+
+	ta.Placeholder = "Send message..."
+	ta.Focus()
+
+	ta.Prompt = "| "
+	ta.CharLimit = 280
+
+	ta.SetWidth(30)
+	ta.SetHeight(1)
+
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+
+	ta.ShowLineNumbers = false
+
+	vp := viewport.New(30, 5)
+	vp.SetContent("Welcome to the chat room!\nType a message and press Enter to send.")
+
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		viewport:    vp,
+		textarea:    ta,
+		messages:    []string{},
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err:         nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		taCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+
+	m.textarea, taCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.viewport.Width = msg.Width
+		m.textarea.SetWidth(msg.Width)
+		m.viewport.Height = msg.Height - m.textarea.Height() - lipgloss.Height(gap)
+
+		if len(m.messages) > 0 {
+			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+		}
+		m.viewport.GotoBottom()
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value())
+			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+			m.textarea.Reset()
+			m.viewport.GotoBottom()
+		}
+	case errorMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	return m, tea.Batch(taCmd, vpCmd)
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s%s%s",
+		m.viewport.View(),
+		gap,
+		m.textarea.View(),
+	)
+}
 
 func main() {
-	fmt.Println("Hello Client!!!")
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("There has been an error: %v", err)
+		os.Exit(1)
+	}
 }
