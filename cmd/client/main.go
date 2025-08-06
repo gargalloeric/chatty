6c61b586dc5054ad59250119ee29b5502d689135
eric <erickmontes555@gmail.com>
2025-08-06 20:27:17 +0200

    feat(client): add initial implementation for server connection

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"net"
 	"os"
 	"strings"
 
@@ -9,21 +11,55 @@ import (
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/gorilla/websocket"
 )
 
 const gap = "\n\n"
 
 type errorMsg error
 
+type receivedMsg struct {
+	Message string
+}
+
+type config struct {
+	host string
+	port string
+}
+
 type model struct {
 	viewport    viewport.Model
 	messages    []string
 	textarea    textarea.Model
 	senderStyle lipgloss.Style
+	conn        *websocket.Conn
+	cfg         config
 	err         error
 }
 
-func initialModel() model {
+func readFromConn(conn *websocket.Conn) tea.Cmd {
+	return func() tea.Msg {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			return errorMsg(err)
+		}
+
+		return receivedMsg{Message: string(message)}
+	}
+}
+
+func writeToConn(conn *websocket.Conn, message string) tea.Cmd {
+	return func() tea.Msg {
+		err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+		if err != nil {
+			return errorMsg(err)
+		}
+
+		return nil
+	}
+}
+
+func initialModel(conn *websocket.Conn) model {
 	ta := textarea.New()
 
 	ta.Placeholder = "Send message..."
@@ -49,12 +85,13 @@ func initialModel() model {
 		textarea:    ta,
 		messages:    []string{},
 		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		conn:        conn,
 		err:         nil,
 	}
 }
 
 func (m model) Init() tea.Cmd {
-	return textarea.Blink
+	return tea.Batch(textarea.Blink, readFromConn(m.conn))
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -81,11 +118,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
 		case tea.KeyEnter:
-			m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value())
+			message := m.textarea.Value()
+			m.messages = append(m.messages, m.senderStyle.Render("You: ")+message)
 			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
 			m.textarea.Reset()
 			m.viewport.GotoBottom()
+			return m, tea.Batch(taCmd, vpCmd, writeToConn(m.conn, message))
 		}
+	case receivedMsg:
+		m.messages = append(m.messages, m.senderStyle.Render("Sender: ")+msg.Message)
+		m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+		m.textarea.Reset()
+		m.viewport.GotoBottom()
+		return m, tea.Batch(taCmd, vpCmd, readFromConn(m.conn))
 	case errorMsg:
 		m.err = msg
 		return m, nil
@@ -104,7 +149,21 @@ func (m model) View() string {
 }
 
 func main() {
-	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	var cfg config
+
+	flag.StringVar(&cfg.host, "addr", "127.0.0.1", "address of the remote server")
+	flag.StringVar(&cfg.port, "port", "3000", "remote server port")
+	flag.Parse()
+
+	path := fmt.Sprintf("ws://%s/v1/ws", net.JoinHostPort(cfg.host, cfg.port))
+
+	conn, _, err := websocket.DefaultDialer.Dial(path, nil)
+	if err != nil {
+		fmt.Printf("There has been an error: %v", err)
+		os.Exit(1)
+	}
+
+	p := tea.NewProgram(initialModel(conn), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("There has been an error: %v", err)
 		os.Exit(1)
